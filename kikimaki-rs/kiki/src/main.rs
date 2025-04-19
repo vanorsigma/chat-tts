e63b8006b723422075cf6409ee9322034fd8a024
vanor-sigma <240724400+vanor-sigma@users.noreply.github.com>
2025-04-19 15:32:43 +0100

    hmm

---
+++ kikimaki-rs/kiki/src/main.rs
@@ -0,0 +1,43 @@
+use shared::{make_pipeline, Settings};
+use rand;
+use tokio::join;
+use tokio_util::sync::CancellationToken;
+
+const PROMPT: &str = "You are a highly intelligent cat named Kiki with a friendly demeanor. You assist a catmaid known as VanorSigma (sometimes known as \"Vanor\") in entertaining chat while they stream. However, you can only react with one-line ASCII cat faces. This is your default face: (='.'=). You should aim to be as creative as possible in your response, using emojis if you need to. However, you cannot reply with text. If you want to animate your responses, leave a new line per cat face. Chat messages come in the form of <username>: <message";
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let api_key = std::env::var("API_KEY").expect("API_KEY not configured");
+    let obs_password = std::env::var("OBS_PASSWORD").expect("OBS_PASSWORD not configured");
+
+    // hard-coded because surely only I use it clueless
+    let settings = Settings {
+        google_api_key: api_key,
+        google_prompt: PROMPT.to_string(),
+        obs_host: "localhost".to_string(),
+        obs_port: 4455,
+        obs_password,
+        obs_source_name: "kiki".to_string(),
+        obs_animation_duration: 200,
+        twitch_target: "vanorsigma".to_string(),
+        twitch_message_to_string: |message| {
+            let num: f32 = rand::random();
+            if num < 0.5 {
+                format!("{0}: {1}", message.username, message.message).into()
+            } else {
+                None
+            }
+        }
+    };
+
+    let cancellation = CancellationToken::new();
+    let pipeline = make_pipeline(settings, cancellation.child_token()).await.expect("can create pipline");
+    log::info!("Created pipeline.");
+
+    tokio::signal::ctrl_c().await.expect("can await for ctrl+c");
+    cancellation.cancel();
+    log::info!("Cancelled.");
+    let _ = join!(pipeline.join_handle);
+}
---
+++ kikimaki-rs/shared/src/twitch.rs
@@ -0,0 +1,92 @@
+use std::process::abort;
+
+use thiserror::Error;
+use tokio::select;
+use tokio::sync::broadcast::{self, Receiver, Sender};
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::task::JoinHandle;
+use tokio_util::sync::CancellationToken;
+use twitch_irc::TwitchIRCClient;
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::message::ServerMessage;
+use twitch_irc::{ClientConfig, SecureTCPTransport};
+
+#[derive(Debug, Error)]
+pub enum TwitchIRCError {
+    #[error("unknown twitch irc error")]
+    Unknown
+}
+
+pub struct TwitchIRCSpawner;
+
+pub struct TwitchIRCConnection {
+    pub handle: JoinHandle<()>,
+    rx: Receiver<TwitchIRCMessage>,
+}
+
+#[derive(Clone)]
+pub struct TwitchIRCMessage {
+    pub username: String,
+    pub message: String,
+}
+
+impl TwitchIRCSpawner {
+    pub async fn connect<S: AsRef<str> + std::marker::Send + 'static>(
+        target: S,
+        cancellation: CancellationToken,
+    ) -> TwitchIRCConnection {
+        let config = ClientConfig::default();
+        let (incoming_messages, client) =
+            TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+        TwitchIRCConnection::new(incoming_messages, client, cancellation, target)
+    }
+}
+
+impl TwitchIRCConnection {
+    fn new<
+        A: twitch_irc::transport::Transport,
+        B: twitch_irc::login::LoginCredentials,
+        S: AsRef<str> + std::marker::Send + 'static,
+    >(
+        mut incoming_message: UnboundedReceiver<ServerMessage>,
+        client: TwitchIRCClient<A, B>,
+        cancellation: CancellationToken,
+        target: S,
+    ) -> Self {
+        let (tx, rx) = broadcast::channel(100);
+        let join_handle = tokio::task::spawn(async move {
+            log::debug!("Trying to join channel {}...", target.as_ref().to_owned());
+            if let Err(e) = client.join(target.as_ref().to_owned()) {
+                log::error!("Error while joining channel: {e}");
+            }
+
+            loop {
+                select! {
+                    Some(message) = incoming_message.recv() => {
+                        log::debug!("Aquired {message:#?}");
+                        if let ServerMessage::Privmsg(msg) = message {
+                            log::debug!("Processing it as {msg:#?}");
+                            let _ = tx.send(TwitchIRCMessage {
+                                username: msg.sender.name.to_string(),
+                                message: msg.message_text.to_string(),
+                            });
+                        }
+                    }
+
+                    _ = cancellation.cancelled() => {
+                        break
+                    }
+                }
+            }
+        });
+
+        TwitchIRCConnection {
+            handle: join_handle,
+            rx,
+        }
+    }
+
+    pub fn subscribe(&self) -> Receiver<TwitchIRCMessage> {
+        self.rx.resubscribe()
+    }
+}
