bc8e38ef07c55695a33f64101512ea8742052451
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2025-05-16 22:34:20 +0100

    fix general bug with warning not playing after audio

--- trinket/src/trinket/frames/shared.py
+++ trinket/src/trinket/frames/shared.py
@@ -3,15 +3,19 @@ Shared Components
 """
 
 from PyQt6.QtWidgets import QTextEdit, QWidget
+from PyQt6.QtGui import QCloseEvent
 from PyQt6.QtCore import Qt, pyqtSignal
 
 class CloseSignalableWidget(QWidget): # pylint: disable=too-few-public-methods
     """
-    A QWidget that emites a closed signal
+    A QWidget that emites a closed signal. It also actually closes
+    QWidget and calls the default behavior
     """
     closed = pyqtSignal(QWidget)
-    def closeEvent(self, event): # pylint: disable=invalid-name,missing-function-docstring
+    def closeEvent(self, event: QCloseEvent) -> None: # pylint: disable=invalid-name,missing-function-docstring
         self.closed.emit(self)
+        self.close()
+        event.accept()
         super().closeEvent(event)
 
 # pylint: disable=too-few-public-methods
