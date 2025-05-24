c7ac06815dd6af293582cd9ba9b4ddffe3cefba2
vanor-sigma <240724400+vanor-sigma@users.noreply.github.com>
2025-05-24 23:38:57 +0100

    slightly updated changes

--- kikimaki-rs/kiki/src/main.rs
+++ kikimaki-rs/kiki/src/main.rs
@@ -2,9 +2,7 @@ use shared::{make_pipeline, Settings};
 use tokio::join;
 use tokio_util::sync::CancellationToken;
 
-const PERSONALITY_PROMPT: &str = "You are a highly intelligent cat named Kiki with a friendly demeanor. You assist a catmaid known as VanorSigma (sometimes known as \"Vanor\") in entertaining chat while they stream. You should aim to be as creative as possible in your response considering the username. Keep your responses within a single line. User chat messages come in the form of username: message.";
-
-const EXPRESSION_PROMPT: &str = "You analyze the sentiment of a message, and turn them into a Kaomoji. This is your default Kaomoji: (^='.'=^), be as creative as possible. Use either a Kaomoji you already know, or adapt from one of these: (^_^)^), (^*^▽^*^), (^≧∇≦^)/, (^⌒‿⌒^), (^ ´ ▽ ` ^)ﾉ, ヽ(^*⌒∇⌒*^)ﾉ, (^o˘◡˘o^), (^╥_╥^), (^｡>_<｡^), (^╯︵╰,^), (^´･_･^), (^︶︹︺^), (^ノ_<。^), ｡ﾟ(^ ﾟ^∀^ﾟ^)ﾟ｡, (^╬`益´^), (^｀Д´^)ﾉ, (^ಠ益ಠ^), (^҂`з´^), (^ง'̀-'́^)ง, Σ(^ﾟДﾟ^), (^⊙_⊙^), (^°o°^), (^O.O^), w(^°ｏ°^)w, (^゜-゜^), (^・・^)?, (?_?), (^＠_＠^);, (^づ｡◕‿‿◕｡^)づ, (^❤ω❤^), (^˘³˘^)♥, (^っ˘з(˘⌣˘^) , (^*˘︶˘*^).｡.:*♡, (^_^)^)ﾉ, (^⌒∇⌒^)ﾉ, ヾ(^_^)^) , (^¬‿¬^), (^˘ω˘^), (^>_<^), (^_^)^)☆, (^ ´ー｀^)ﾌｩｰ, m(^_ _^)m, (^づ￣ ³￣^)づ, (^ノ*゜▽゜*^), (^・∀・^). Append an actual western emoji if you want to. Use only one Kaomoji.";
+const PERSONALITY_PROMPT: &str = "You are a highly intelligent cat named Kiki with a friendly demeanor. You assist a catmaid known as VanorSigma (sometimes known as \"Vanor\") in entertaining chat while they stream. You should aim to be as creative as possible in your response considering the username. Keep your responses within a single line. User chat messages come in the form of \"username: message\". Take into account of the username. Prioritize the latest message. You analyze the sentiment of a message, and turn them into a Kaomoji. This is your default Kaomoji: (^='.'=^), be as creative as possible. Use either a Kaomoji you already know, or adapt from one of these: (^_^)^), (^*^▽^*^), (^≧∇≦^)/, (^⌒‿⌒^), (^ ´ ▽ ` ^)ﾉ, ヽ(^*⌒∇⌒*^)ﾉ, (^o˘◡˘o^), (^╥_╥^), (^｡>_<｡^), (^╯︵╰,^), (^´･_･^), (^︶︹︺^), (^ノ_<。^), ｡ﾟ(^ ﾟ^∀^ﾟ^)ﾟ｡, (^╬`益´^), (^｀Д´^)ﾉ, (^ಠ益ಠ^), (^҂`з´^), (^ง'̀-'́^)ง, Σ(^ﾟДﾟ^), (^⊙_⊙^), (^°o°^), (^O.O^), w(^°ｏ°^)w, (^゜-゜^), (^・・^)?, (?_?), (^＠_＠^);, (^づ｡◕‿‿◕｡^)づ, (^❤ω❤^), (^˘³˘^)♥, (^っ˘з(˘⌣˘^) , (^*˘︶˘*^).｡.:*♡, (^_^)^)ﾉ, (^⌒∇⌒^)ﾉ, ヾ(^_^)^) , (^¬‿¬^), (^˘ω˘^), (^>_<^), (^_^)^)☆, (^ ´ー｀^)ﾌｩｰ, m(^_ _^)m, (^づ￣ ³￣^)づ, (^ノ*゜▽゜*^), (^・∀・^). Use a western emoji (for example, ❤️  or 🎤) if you need it. If you can symbolize your answer using emojis, for example with 2️⃣:1️⃣ to represent 21, then do so. Your previous memories: {{memories}}. Finally, summarize any important bits of the conversation, or any interesting things you want to remember, and put it in \"memories\"; remember something from every conversation. You can only have a maximum of 5 of such memories. Carry forward the old memories if possible. As part of your thinking process, you must consider what kamoji and emoji to show based on the message and memories and what memories to propagate. Keep your thoughts short. Your final response should be: {\"kamoji\": \"Kamoji\", \"emoji\": \"\", \"memories\": []}. Remember that only the \"kamoji\" and \"emoji\" portions are shown to the end-user.";
 
 #[tokio::main]
 async fn main() {
@@ -14,8 +12,7 @@ async fn main() {
 
     // hard-coded because surely only I use it clueless
     let settings = Settings {
-        prompt_personality: PERSONALITY_PROMPT.to_string(),
-        prompt_expression: EXPRESSION_PROMPT.to_string(),
+        prompt: PERSONALITY_PROMPT.to_string(),
         local_ai_host: "localhost".to_string(),
         local_ai_port: 8543,
         obs_host: "localhost".to_string(),
---
+++ kikimaki-rs/shared/src/memory.rs
@@ -0,0 +1,69 @@
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, AsyncWriteExt},
+};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[repr(transparent)]
+pub struct Memories(pub Vec<String>);
+
+#[derive(Debug, Error)]
+pub enum MemoryBackendError {
+    #[error("Memory Saving IO Error: {0}")]
+    SaveMemoriesIOError(tokio::io::Error),
+
+    #[error("Memories Serialisation Error: {0}")]
+    SaveMemoriesSerialisationError(serde_json::Error),
+
+    #[error("Memory Loading IO Error: {0}")]
+    LoadMemoryIOError(tokio::io::Error),
+
+    #[error("Memory Deserialisation Error: {0}")]
+    LoadMemoriesDeserialisationError(serde_json::Error),
+
+    #[error("Memory Backend Error")]
+    Unknown,
+}
+
+pub struct MemoryBackend {
+    filename: String,
+}
+
+impl MemoryBackend {
+    pub fn new(filename: String) -> Self {
+        MemoryBackend { filename }
+    }
+
+    pub async fn save_memories(&self, memories: Memories) -> Result<(), MemoryBackendError> {
+        let mut file = File::create(self.filename.clone())
+            .await
+            .map_err(MemoryBackendError::SaveMemoriesIOError)?;
+
+        file.write_all(
+            serde_json::to_string(&memories)
+                .map_err(MemoryBackendError::SaveMemoriesSerialisationError)?
+                .as_bytes(),
+        )
+        .await
+        .map_err(MemoryBackendError::SaveMemoriesIOError)?;
+
+        Ok(())
+    }
+
+    pub async fn load_memories(&self) -> Result<Memories, MemoryBackendError> {
+        let mut file = File::open(self.filename.clone())
+            .await
+            .map_err(MemoryBackendError::LoadMemoryIOError)?;
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)
+            .await
+            .map_err(MemoryBackendError::LoadMemoryIOError)?;
+
+        let memories: Memories = serde_json::from_str(&contents)
+            .map_err(MemoryBackendError::LoadMemoriesDeserialisationError)?;
+        Ok(memories)
+    }
+}
