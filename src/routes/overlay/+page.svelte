0c594960e63e107f9ab36d4929a757e372b8ccba
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2025-09-12 13:41:57 -0700

    oh i forgot to commit he mistake, also heart rate integration

--- src/routes/overlay/+page.svelte
+++ src/routes/overlay/+page.svelte
@@ -2,7 +2,7 @@
   import { onMount } from 'svelte';
   import { ChatBulletContainer } from './chatbullet';
   import { createNewAuthenticatedSelfTwitchClient, createNewTwitchClient } from '$lib/twitch';
-  import { PUBLIC_TWITCH_OAUTH, PUBLIC_BUS_URL } from '$env/static/public';
+  import { PUBLIC_TWITCH_OAUTH, PUBLIC_BUS_URL, PUBLIC_HEARTRATE_URL } from '$env/static/public';
   import { OverlayDispatchers } from './dispatcher';
   import { BLACK_SILENCE_DURATION, Commands } from './commands';
   import {
@@ -13,8 +13,11 @@
     mistakeStore
   } from './stores.svelte';
   import { CaptchaObserver } from './captcha';
+  import { Heartrate } from './heartrate';
 
   export let chatBulletContainer: HTMLDivElement;
+  let heartrate = new Heartrate(PUBLIC_HEARTRATE_URL);
+
   let flashbangCount: number = 0;
   let blackSilenceCount: number = 0;
   let client = createNewAuthenticatedSelfTwitchClient('vanorsigma', PUBLIC_TWITCH_OAUTH);
@@ -179,7 +182,7 @@
   {#if blackSilenceBorder}
     <div class="blackSilenceBorder"></div>
   {/if}
-  {#if blackSilenceCount < blackSilenceStore.count}<div class="fullscreenvideo">
+  {#if blackSilenceCount < blackSilenceStore.count}<div class="fullscreenvideo blacksilence">
       <!-- svelte-ignore a11y_media_has_caption -->
       <video autoplay onended={onBlackSilenceDone} onplaying={onBlackSilenceStart}>
         <source src="/blacksilence.webm" /> Video tag smile
@@ -200,22 +203,49 @@
         <source src="/thinkfast.webm" /> Video tag smile
       </video>
     </div>{/if}
-  {#if pollStore.data}
-    <div class="poll-box">
-      <h3>{pollStore.data?.title}</h3>
-      {#each pollStore.data?.options ?? [] as option, idx}
-        <div class="option">
-          <div class="option-title">{option.name} [type {idx + 1}] ({option.votes} votes)</div>
-          <div class="progress-container">
-            <div
-              class="progress-bar"
-              style="width: {(option.votes / pollStore.totalVotes) * 100}%;"
-            ></div>
+  <div class="rightpanel">
+    {#if pollStore.data}
+      <div class="poll-box">
+        <h3>{pollStore.data?.title}</h3>
+        {#each pollStore.data?.options ?? [] as option, idx}
+          <div class="option">
+            <div class="option-title">{option.name} [type {idx + 1}] ({option.votes} votes)</div>
+            <div class="progress-container">
+              <div
+                class="progress-bar"
+                style="width: {(option.votes / pollStore.totalVotes) * 100}%;"
+              ></div>
+            </div>
           </div>
-        </div>
-      {/each}
+        {/each}
+      </div>
+    {/if}
+    <div class="heartrate">
+      <svg
+        width="70.90000000000006"
+        height="70.90000000000006"
+        viewBox="-720 -750 1500 1450"
+        style="margin: 0px 30px 0px 0px; filter: none;"
+        ><path
+          transform="scale(25)"
+          style="transform-origin: center center 0px; transform-box: fill-box; stroke-width: 0px;"
+          fill="#f72d21ff"
+          d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
+  c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
+  c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
+  C52.216,18.553,51.97,16.611,51.911,16.242z"
+        ></path><animateTransform
+          attributeName="transform"
+          type="scale"
+          values="0.8; 1.1; 1; 1.1; 1.1; 0.8;"
+          dur="500ms"
+          repeatCount="indefinite"
+          additive="sum"
+        ></animateTransform></svg
+      >
+      <p>{$heartrate}</p>
     </div>
-  {/if}
+  </div>
 </div>
 
 <style>
@@ -297,11 +327,36 @@
     overflow: none;
   }
 
-  .poll-box {
+  .rightpanel {
+    display: flex;
+    flex-direction: column;
     position: absolute;
-    top: 40px;
-    right: 10px;
+    top: 0px;
+    right: 0px;
+    height: 100%;
     width: 400px;
+    padding-top: 40px;
+    padding-right: 10px;
+  }
+
+  .heartrate {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 200px;
+    color: rgb(4, 187, 175);
+    font-family: 'Fredoka One';
+    font-size: 72px;
+    font-weight: bold;
+  }
+
+  .heartrate p {
+    padding: 0;
+    margin: 0;
+  }
+
+  .poll-box {
+    width: 90%;
     padding: 10px;
     border: 1px solid #ccc;
     border-radius: 5px;
